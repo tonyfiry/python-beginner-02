284b9052e4fbfd1ec81ed5d9d6b7603d6c979c9e
tonyfiry <tbo79435@gmail.com>
2022-06-05 00:19:27 +0800

    homework

--- set.py
+++ set.py
@@ -12,6 +12,7 @@
 # print(list1)#顯示['曾山水', '鄧美麗', '林小明']
 # print(list1[2])#林小明
 
+##集合的操作
 #&交集:是兩個不同集合，重點它是重複的集合
 # set1 = {1,2,4,10,9}
 # set2 = {1,2,3,4,5,6}
@@ -33,6 +34,8 @@
 # B = {3,4,5,6}
 # print(A^B)
 
+
+##集合的方法
 #set(add)方法:
 # class01 = {"二年甲班","三年甲班","四年乙班"}
 # class01.add("一年甲班")
@@ -55,8 +58,58 @@
 # print(class01)
 
 #set(remove)方法:
-class01 = {"dive","tony","daia"}
-class01.remove("dive")
-print(class01)
+# class01 = {"dive","tony","daia"}
+# class01.remove("dive")
+# print(class01)
 
 #set(pop)方法:
+# name01 = {"nana","diva","maod","mair"}
+# p = name01.pop()
+# print(p)#列印nana
+# print(name01)#列印{'diva', 'mair', 'maod'}
+
+# #set(update)方法:
+# A = {1,2,3,4,5,6}
+# B = {2,3,4,5,6,7,8,9}
+# A.update(B)
+# print(A)#列印{1, 2, 3, 4, 5, 6, 7, 8, 9}
+# print(B)#列印{2, 3, 4, 5, 6, 7, 8, 9}
+
+##集合基本函式
+#max函式:
+# A = {1,2,3,4}
+# print(max(A))#最大是4
+# #min函式:
+# print(min(A))#最小是1
+# #sum函式:
+# print(sum(A))#總和是10
+# #len函式:
+# print(len(A))#長度是4
+# print(enumerate(A))#enumerate物件
+
+# for item in list(enumerate(A)):
+#     print(item)#(0, 1),(1, 2),(2, 3),(3, 4)
+
+# for i,item in enumerate(A):
+#     print(i,item)# 0 1 : 1 2 : 2 3 : 3 4 
+
+#frozenset(凍結集合):
+
+
+A = frozenset({1,2,3,4})
+B = frozenset({3,4,5,6})
+
+print(A&B)
+print(A|B)
+print(A-B)
+print(A^B)
+print(A in B)
+print(A not in B)
+print(A != B)
+print(A == B)
+
+print(max(A))
+print(min(B))
+
+
+
